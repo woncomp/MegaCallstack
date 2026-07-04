2df499f7709230d1bc51542416b790712bd9ad93
Joshua Zhang <woncomp@gmail.com>
2026-07-04 13:43:02 +0800

    Extract HomePage UI into a dedicated HomePageControl

---
+++ ToolWindows/HomePageControl.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MegaCallstack.ToolWindows
+{
+    public partial class HomePageControl : UserControl
+    {
+        public HomePageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
